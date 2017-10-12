d5220fb901c57343c03b1dc772df03ec82f30b74
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-10-12 09:46:15 -0700

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -2,7 +2,7 @@ from flask import Flask, url_for, render_template, request
 
 app = Flask(__name__) #__name__ = "__main__" if this is the file that was run.  Otherwise, it is the name of the file (ex. webapp)
 
-@app.route("/")
+@app.route(".")
 def render_main():
     return render_template('index.html')
 
@@ -15,7 +15,6 @@ def render_response():
     return render_template('response.html', response = reply)
 if __name__=="__main__":
     app.run(debug=False, port=54321)
-    @app.route("/me2mi")
 @app.route("/me2mi")
 def render_page1():
     return render_template('templates/meterToMiles.html')
