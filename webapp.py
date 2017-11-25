9b5068ed4127ba5d984767565ad81e20e0c31d72
Nick Noormand <30534503+Venomspiter@users.noreply.github.com>
2017-11-25 13:49:02 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,19 +13,13 @@ def render_response():
     return render_template('response.html', response = reply)
 @app.route("/metersToMiles")
 def render_page1():
-    length = Float(request.args['length']* .00062137)
-    reply = "your new length is" + length + "."
-    return render_template('metersToMiles.html', response = reply)
+    return render_template('metersToMiles.html')
 
 @app.route("/milesToKilos")
 def render_page2():
-    length = Float(request.args['length']* 1.60934)
-    reply = "your new length is" + length + "."
-    return render_template('milesToKilos.html', response = reply)
+    return render_template('milesToKilos.html')
 @app.route("/kilosToMiles")
 def render_page3():
-    length = Float(request.args['length']*  0.621371)
-    reply = "your new length is" + length + "."
-    return render_template('kilosToMiles.html', response = reply)
+    return render_template('kilosToMiles.html')
 if __name__=="__main__":
     app.run(debug=False, port=54321)
